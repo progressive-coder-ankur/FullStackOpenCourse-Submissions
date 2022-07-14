3750c3715d16faf4242aa9b531bcffdf1bcf7e3e
Ankur Singh <abishek.singh.chauhan@gmail.com>
2022-07-14 17:10:35 +0545

    part 3 : exercises 3.1-3.8

--- part3/node/index.js
+++ part3/node/index.js
@@ -1,6 +1,9 @@
 const { response } = require('express');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
 let notes = [
   {
     id: 1,
@@ -22,6 +25,11 @@ let notes = [
   },
 ];
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
+  return maxId + 1;
+};
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 });
@@ -36,13 +44,32 @@ app.get('/api/notes/:id', (request, response) => {
   note ? response.json(note) : response.status(404).end();
 });
 
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+  response.json(note);
+});
+
 app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id;
+  const id = Number(request.params.id);
   notes = notes.filter(note => note.id !== id);
   response.status(204).end();
 });
 
-const PORT = 3434;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log('Server running on ' + PORT);
 });
